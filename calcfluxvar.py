2e12c55da3d2a69733525785a32f198ada57f423
Sean Goebel <geekyrocketguy@gmail.com>
2018-08-10 17:03:38 -1000

    Forgot to add these before.

---
+++ calcfluxvar.py
@@ -0,0 +1,140 @@
+import numpy as np
+import astropy.io.fits as pyfits
+import matplotlib.pyplot as plt
+import os
+import pdb
+
+def main(infilename, showframes=True, checkframedrops=True):
+
+#Syntax:
+#calcfluxvar.main('mylistoffiles.txt', showframes=True, checkframedrops=True)
+#
+#See files.txt for an example of how to structure the input file list file.
+#It should be structured as
+#[PRV voltage] PRV
+#[Common voltage] [filename (directory optional)]
+#e.g. 10 ~/Desktop/myfile.fits
+#Comments can be indicated with a #
+#e.g. 3.5910 PRV
+#If all files are in one directory, that can be specified after a 99.
+#Otherwise it is assumed the directory info is in the filename.
+#
+#showframes=True will display the mean and variance frames for the user before
+# saving them. Set =False for faster code operation.
+#checkframedrops=True plots the mean of each frame in order to see whether
+# resets are occurring where they should. Set checkframedrops=False for faster
+# code operation.
+    
+    junk = np.loadtxt('files.txt', dtype='str')
+    volts = junk[:,0].astype(float)
+    filenames= junk[:,1]
+
+    #How many frames are in each ramp?
+    reset_freq = 300
+
+    #How many ramps are there?
+    n_ramps = 100
+    
+    #Now, define the frame numbers in each ramp that should be used to produce the cds
+    # pairs. Frames between cds_f1 and cds_f2 are averaged to reduce the read noise,
+    # and that is subtracted from the average of frames cds_f3 through cds_f4.
+    cds_f1 = 20
+    cds_f2 = 120
+    cds_f3 = 180
+    cds_f4 = 280
+
+    if 99 in volts:
+        dir = filenames[np.squeeze(np.where(volts == 99))]
+        filenames = np.delete(filenames, np.squeeze(np.where(volts == 99)))
+        volts = np.delete(volts, np.squeeze(np.where(volts == 99)))
+    else:
+        dir=''
+
+    #Print stuff out to the user to confirm it.
+    lowercase_filenames = [v.lower() for v in filenames]
+    if 'prv' in lowercase_filenames:
+        prv=volts[lowercase_filenames.index('prv')]
+        volts = np.delete(volts, lowercase_filenames.index('prv'))
+        filenames = np.delete(filenames, lowercase_filenames.index('prv'))
+    else:
+        print('PRV not specified in'+infilename+'.')
+        prv=input('Please specify PRV\n')
+        print('Thank you. In the future, this can be avoided by adding to the text file "[value] PRV"')
+
+    print('PRV =', prv)
+    print()
+
+    print("The data read was")
+    for i in range(len(volts)):
+        print( volts[i], dir+filenames[i])
+    print()
+        
+    print("Frames", cds_f1,"through", cds_f2, "will be averaged and subtracted from the average of frames", cds_f3, "through", cds_f4)
+    print()
+
+    for i in np.arange(1, 2):#range(len(filenames)):
+        print("Working on file", i+1, "of", len(filenames))
+        img = pyfits.getdata(dir+filenames[i])
+
+        if not os.path.isdir('flux_var_files'):
+            os.mkdir('flux_var_files')
+            print('Created flux_var_files/ directory to store images.')
+
+        imcube = np.zeros((n_ramps, np.shape(img)[1], np.shape(img)[2]))
+
+        #populate the cube of cds pairs
+        for j in range(n_ramps):
+            imcube[j] = np.mean(img[j*reset_freq + cds_f3 : j*reset_freq + cds_f4], 0) - \
+                        np.mean(img[j*reset_freq + cds_f1 : j*reset_freq + cds_f2], 0) 
+
+        average = np.mean(imcube, 0)
+        variance = np.var(imcube, axis=0, ddof=1)
+
+        if checkframedrops: #plot median frame values and where resets should occur
+            meds = np.zeros(len(img))
+            for j in range(len(img)):
+                meds[j] = np.median(img[j, 64:-64, 64:-64])
+            plt.plot(meds, 'o')
+            y0 = np.min(meds)
+            y1 = np.max(meds)
+            for j in range(n_ramps):
+                plt.plot([reset_freq*j, reset_freq*j], [y0, y1], 'r-')
+            plt.title('Median of Each Frame')
+            plt.ylabel('ADUs')
+            plt.xlabel('Frame number')
+            plt.show()
+        
+        if showframes: #Display average and variance frames
+            plt.figure(1, figsize=(10, 4), dpi=100)
+
+            plt.suptitle(filenames[i])
+            
+            plt.subplot(121)
+            #select min and max for scaling, presently 4% and 96%
+            avgmin = np.sort(average.flatten())[int(round(0.04 * np.size(average)))]
+            avgmax = np.sort(average.flatten())[int(round(0.96 * np.size(average)))]
+            plt.imshow(average, interpolation='none', vmin=avgmin, vmax=avgmax)
+            plt.title('Average')
+            plt.colorbar(shrink=0.7)
+
+            plt.subplot(122)
+            #select min and max for scaling, presently 4% and 96%
+            varmin = np.sort(variance.flatten())[int(round(0.01 * np.size(average)))]
+            varmax = np.sort(variance.flatten())[int(round(0.99 * np.size(average)))]
+            plt.imshow(variance, interpolation='none', vmin=varmin, vmax=varmax)
+            plt.title('Variance')
+            plt.colorbar(shrink=0.7)
+
+            plt.tight_layout()
+            plt.show()
+            
+
+        #save images
+        if '/' in filenames[i]:
+            #strip off directory information
+            filenames[i] = filenames[i][filenames[i].rfind('/') : ]
+        newfilename=filenames[i][:filenames[i].find('.fits')] #strip '.fits'
+        pyfits.writeto('flux_var_files/'+newfilename+'_avg.fits', average, overwrite=True)
+        print('Wrote flux_var_files/'+newfilename+'_avg.fits')
+        pyfits.writeto('flux_var_files/'+newfilename+'_var.fits', variance, overwrite=True)
+        print('Wrote flux_var_files/'+newfilename+'_var.fits')
--- framestocube.py
+++ framestocube.py
@@ -6,13 +6,14 @@ import sys
 
 def main(filestub='', outname='', first=-1, last=-1):
     if (filestub=='') | (outname==''):
-        print "USAGE: framestocube.main('filestub', 'outputname', firstframe, lastframe')"
-        print "The filestub should include all characters except the frame number and .fits."
-        print "If the last two numbers are not included, all frames matching the stub will be processed into a cube."
+        print( "USAGE: framestocube.main(filestub='filestub', outname='outputname', firstframe, lastframe')")
+        print( "The filestub should include all characters except the frame number and .fits.")
+        print( "If the last two numbers are not included, all frames matching the stub will be processed into a cube.")
+        return
 
     if ((first == -1) & (last != -1)) | \
        ((first != -1) & (last == -1)):
-       print "Please specify first and last frames"
+       print( "Please specify first and last frames")
 
     files = glob.glob(filestub+'*.fits') #get list of filenames
 
@@ -20,7 +21,7 @@ def main(filestub='', outname='', first=-1, last=-1):
     for i in range(len(files)):
         filenums[i] = framenum(files[i], filestub)
 
-    print len(files), "files found."
+    print( len(files), "files found.")
 
     if len(files)==0: return #nothing found, therefore quit
 
@@ -31,7 +32,7 @@ def main(filestub='', outname='', first=-1, last=-1):
     for i in range(len(files)):
         myframenum = framenum(files[i], filestub)
         if i%int(len(files)/100)==0:
-            print str(int(round(float(i)/len(files)*100.)))+"% done."
+            print( str(int(round(float(i)/len(files)*100.)))+"% done.")
             
         if (myframenum >= first) & (myframenum <= last):
             if 'cube' not in locals(): #first time, initialize cube
@@ -43,7 +44,7 @@ def main(filestub='', outname='', first=-1, last=-1):
             #print "file, layer", files[i], myframenum-first
 
     pyfits.writeto(outname, cube.astype('uint16'), overwrite=True)
-    print "Wrote file", outname
+    print( "Wrote file", outname)
 
 
 def framenum(filename, filestub):
@@ -55,15 +56,15 @@ def framenum(filename, filestub):
     
 def batch(filestub2='', outname='', reset_freq=0):
     if filestub2=='' or outname=='' or reset_freq==0:
-        print "Syntax: framestocube.batch(filestub, outname, reset_freq) \n"+\
-            " e.g. framestocube.batch('blah-3-', 'mycubes.fits', 320)"
+        print( "Syntax: framestocube.batch(filestub, outname, reset_freq) \n"+\
+            " e.g. framestocube.batch('blah-3-', 'mycubes.fits', 320)")
         return
 
     files = glob.glob(filestub2+'*.fits') 
     if len(files)%reset_freq != 0:
-        print "Total number of frames is not divisible by reset frequency."
-        print "N_frames:", len(files)
-        print
+        print( "Total number of frames is not divisible by reset frequency.")
+        print( "N_frames:", len(files))
+        print()
         return
     
     for n in range(len(files)/reset_freq):
